0f73b99eeb4d2300f744234da8271b6906b1220f
Begachu <ygo65312@naver.com>
2022-11-11 22:04:14 +0900

    complete select-box component

--- src/components/common/SelectBox.vue
+++ src/components/common/SelectBox.vue
@@ -5,15 +5,17 @@
       <font-awesome-icon class="down-icon" icon="fa-solid fa-chevron-down" />
     </div>
     <template v-if="open">
-      <div
-        v-for="(item, idx) of options"
-        :key="`option-key-${idx}`"
-        class="option"
-        :class="{ selected: select === item }"
-        @click="changeOption(item)"
-      >
-        {{ item }}
-      </div>
+      <ul class="scroll">
+        <li
+          v-for="(item, idx) of options"
+          :key="`option-key-${idx}`"
+          class="option"
+          :class="{ selected: select === item }"
+          @click="changeOption(item)"
+        >
+          {{ item }}
+        </li>
+      </ul>
     </template>
   </div>
 </template>
@@ -21,11 +23,13 @@
 <script>
 export default {
   name: "SelectBox",
+  props: {
+    select: String,
+    options: Array,
+  },
   data() {
     return {
       open: false,
-      select: "test1",
-      options: ["test1", "test2", "test3", "test4", "test5", "test6"],
     };
   },
 
@@ -34,8 +38,7 @@ export default {
       this.open = !this.open;
     },
     changeOption(option) {
-      this.select = option;
-      this.open = false;
+      this.$emit("on-change", option);
     },
   },
 };
@@ -51,15 +54,21 @@ export default {
   background-color: var(--white);
   padding: 4px;
   position: relative;
+  transition: all 0.1s linear;
 }
-.down-icon {
+.select-container .down-icon {
   position: absolute;
   right: 10px;
   width: 20px;
   top: 10px;
+  transition: all 0.1s linear;
+}
+.select-container.open .down-icon {
+  transform: rotate(180deg);
 }
 .select-container.open {
-  height: auto;
+  height: 200px;
+  overflow: hidden;
 }
 .select-container.open .select {
   background-color: var(--gray);
@@ -69,15 +78,39 @@ export default {
   width: 100%;
   border-radius: 20px;
   border: 0;
-  line-height: 26px;
+  line-height: 28px;
   padding: 0 10px;
   color: var(--navy);
+  transition: all 0.1s linear;
+}
+.scroll {
+  padding: 0;
+  width: 100%;
+  height: 150px;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  margin-top: 5px;
+}
+.scroll::-webkit-scrollbar {
+  position: absolute;
+  border-radius: 3px;
+  width: 6px;
+  background-color: var(--white);
+}
+.scroll::-webkit-scrollbar-thumb {
+  width: 6px;
+  background-color: var(--navy);
+  border-radius: 3px;
+
 }
 .option {
+  box-sizing: border-box;
+  width: 100%;
   color: var(--navy);
   font-size: 16px;
   line-height: 28px;
   padding: 1px 10px;
+  list-style: none;
 }
 .option:hover {
   background-color: var(--gray);
