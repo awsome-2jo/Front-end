65f9c26f901f4689cf847b3c0b9dad1aa7529b51
Begachu <ygo65312@naver.com>
2022-11-13 17:56:02 +0900

    add test component : multi-slider

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,12 @@
     {{ text }}
     <toggle-button :value="bool" @event="setBool" />
     <checkbox-button :value="bool" @event="setBool" text="연습 체크박스" />
-    <select-box />
+    <select-box :select="select" :options="options" @on-change="setSelect" />
+    <multi-slider :_min="min" :_max="max" :unit="unit" unitText="만원" @set-value="setValue" :request="request" />
+
+    <round-button @event="sendRequest" text="값 받기"/>
+    <p>min : {{min}}</p>
+    <p>max : {{max}}</p>
   </div>
 </template>
 
@@ -13,14 +18,22 @@ import TextInput from "@/components/common/TextInput";
 import ToggleButton from "../components/common/ToggleButton.vue";
 import CheckboxButton from "../components/common/CheckboxButton.vue";
 import SelectBox from "../components/common/SelectBox.vue";
+import MultiSlider from "@/components/common/MultiSlider.vue";
+import RoundButton from "@/components/common/RoundButton.vue";
 
 export default {
-  components: { TextInput, ToggleButton, CheckboxButton, SelectBox },
+  components: { TextInput, ToggleButton, CheckboxButton, SelectBox, MultiSlider, RoundButton },
   name: "HomeView",
   data() {
     return {
       text: "",
       bool: false,
+      select: "default",
+      options: ["속성 1","속성 2","속성 3","속성 4","속성 5","속성 6","속성 7",],
+      min: 8000,
+      max: 120000,
+      unit: 1000,
+      request: false,
     };
   },
   methods: {
@@ -30,6 +43,17 @@ export default {
     setBool() {
       this.bool = !this.bool;
     },
+    setSelect(select) {
+      this.select = select;
+    },
+    setValue(min, max){
+      this.min = min;
+      this.max = max;
+      this.request = false;
+    },
+    sendRequest() {
+      this.request = true;
+    }
   },
 };
 </script>
