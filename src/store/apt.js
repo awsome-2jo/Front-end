04203c5badeff3b5fdf8ff97423f2d8404f43461
한유경 <ygo65312@naver.com>
2022-11-15 14:35:29 +0900

    add vuex for apt

---
+++ src/store/apt.js
@@ -0,0 +1,108 @@
+import http from "@/apis/http";
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    sido: "광역시/도",
+    gugun: "시/군/구",
+    dong: "동/읍/면",
+    sidoList: [],
+    gugunList: [],
+    dongList: [],
+    regcode: "",
+  },
+  getters: {},
+  mutations: {
+    SET_REGCODE(state, val) {
+      state.regcode = val;
+    },
+    SET_SIDO(state, val) {
+      if (!val) val = "광역시/도";
+      state.sido = val;
+    },
+    SET_GUGUN(state, val) {
+      console.log("SET_GUGUN");
+      if (!val) val = "시/군/구";
+      state.gugun = val;
+    },
+    SET_DONG(state, val) {
+      if (!val) val = "동/읍/면";
+      state.dong = val;
+    },
+    SET_SIDO_LIST(state, arr) {
+      if (!arr) arr = [];
+      state.sidoList = arr;
+    },
+    SET_GUGUN_LIST(state, arr) {
+      if (!arr) arr = [];
+      state.gugunList = arr;
+    },
+    SET_DONG_LIST(state, arr) {
+      if (!arr) arr = [];
+      state.dongList = arr;
+    }
+  },
+  actions: {
+    /************* 지역 변경 메서드 **************/
+    // 광역시/도 변경 메서드
+    setSido(context, data) {
+      // 변경한 값 설정
+      if (data) context.commit("SET_REGCODE", data.code);
+      context.commit("SET_SIDO", data?.text);
+      context.dispatch("setGugunList", data?.code);
+      context.dispatch("setGugun");
+    },
+
+    // 시/구/군 변경 메서드
+    setGugun(context, data) {
+      if (data) context.commit("SET_REGCODE", data.code);
+      context.commit("SET_GUGUN", data?.text);
+      context.dispatch("setDongList", data?.code);
+      context.dispatch("setDong");
+    },
+
+    // 동/읍/면 변경 메서드
+    setDong(context, data) {
+      if (data) context.commit("SET_REGCODE", data.code);
+      context.commit("SET_DONG", data?.text);
+    },
+
+
+    /************** 지역 목록 메서드 **************/
+    // 광역시/도 목록 메서드
+    setSidoList(context) {
+      http.get(`apt/sido`)
+        .then(res => context.dispatch("setOptList", res.data))
+        .then(data => context.commit("SET_SIDO_LIST", data));
+    },
+
+    // 시/구/군 목록 메서드
+    setGugunList(context, val) {
+      http.get(`apt/gugun?code=${val}`)
+        .then(res => context.dispatch("setOptList", res.data))
+        .then(data => context.commit("SET_GUGUN_LIST", data));
+    },
+
+    // 동/읍/면 목록 메서드
+    setDongList(context, val) {
+      http.get(`apt/dong?code=${val}`)
+        .then(res => context.dispatch("setOptList", res.data))
+        .then(data => context.commit("SET_DONG_LIST", data));
+    },
+
+    /************** ETC **************/
+    // option box에 넣기 위해 속성값 변경
+    setOptList(context, data) {
+      for (let item of data) {
+        item.text = item.name;
+        delete item.name;
+      }
+      return data;
+    }
+  },
+  modules: {
+  }
+})
--- src/views/AptView.vue
+++ src/views/AptView.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <main class="apt-component">
     <filter-nav />
     <kakao-map />
     <side-drop />
@@ -7,14 +7,34 @@
 </template>
 
 <script>
-import FilterNav from "../components/apt/FilterNav.vue";
-import SideDrop from "../components/apt/SideDrop.vue";
-import KakaoMap from "../components/apt/KakaoMap.vue";
+import FilterNav from "@/components/apt/FilterNav.vue";
+import SideDrop from "@/components/apt/SideDrop.vue";
+import KakaoMap from "@/components/apt/KakaoMap.vue";
+import store from "@/store/apt.js";
 
 export default {
   components: { SideDrop, FilterNav, KakaoMap },
   name: "AptMap",
+  store,
+  created() {
+    this.$store.dispatch("setSidoList");
+  },
 };
 </script>
 
-<style></style>
+<style>
+/* 상단 그림자 NAV에 그림자 추가 */
+.apt-component::before {
+  content: "";
+  width: 100%;
+  height: 60px;
+  z-index: 7999;
+  border-bottom: 1px solid var(--gray);
+  box-shadow: 0px 0px 10px var(--shadow);
+  display: flex;
+  padding: 10px;
+  box-sizing: border-box;
+  position: absolute;
+  background-color: var(--white);
+}
+</style>
