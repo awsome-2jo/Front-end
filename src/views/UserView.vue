39d23d43058dc4fcc43f4acd172876c0e1b821d2
Begachu <ygo65312@naver.com>
2022-11-25 01:35:33 +0900

    마이페이지 추가

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -2,9 +2,13 @@
   <main>
     <div class="info-container">
       <h2>USER INFO</h2>
-      <div class="btn-container">
-        <round-button type="line" text="회원탈퇴"/>
-        <round-button  text="정보수정"/>
+      <div class="btn-container" v-if="defaultMode">
+        <round-button type="line" text="회원탈퇴" @event="deleteUser"/>
+        <round-button  text="회원수정" @event="setMode(`modify`)"/>
+      </div>
+      <div class="btn-container" v-else>
+        <round-button type="line" text="수정취소" @event="setMode(`default`)"/>
+        <round-button  text="수정적용" @event="modifyUser()"/>
       </div>
       <div class="flex">
         <div class="user-img">
@@ -20,7 +24,12 @@
           <div>
             <font-awesome-icon icon="fa-solid fa-signature" class="icon" />
             <label>이름</label>
-            <input type="text" v-model="user.name" placeholder="-" disabled>
+            <input type="text" v-model="user.name" placeholder="-" :disabled="defaultMode">
+          </div>
+          <div>
+            <font-awesome-icon icon="fa-solid fa-key" class="icon" />
+            <label>비밀번호</label>
+            <input type="password" v-model="user.pass" placeholder="*************" :disabled="defaultMode">
           </div>
           <div>
             <font-awesome-icon icon="fa-solid fa-envelope" class="icon" />
@@ -30,32 +39,39 @@
           <div>
             <font-awesome-icon icon="fa-solid fa-phone" class="icon" />
             <label>전화번호</label>
-            <input type="phone" v-model="user.phone" placeholder="-" disabled>
+            <input type="phone" v-model="user.phone" placeholder="-" :disabled="defaultMode">
           </div>
           <div>
             <font-awesome-icon icon="fa-solid fa-venus-mars" class="icon" />
             <label>성별</label>
-            <input type="phone" placeholder="-" :value="user.age==1?`남자`:user.age==2 ?`여자`:``" disabled>
-            <input v-if="mode===`modify`" type="radio">
+            <input v-if="defaultMode" type="phone" placeholder="-" :value="genderString" disabled>
+            <div v-else class="radio">
+              <font-awesome-icon icon="fa-solid fa-venus-mars"/>
+              <input type="radio" v-model="user.gender" value="0">
+              <font-awesome-icon icon="fa-solid fa-mars" />
+              <input type="radio" v-model="user.gender" value="1">
+              <font-awesome-icon icon="fa-solid fa-venus" />
+              <input type="radio" v-model="user.gender" value="2">
+            </div>
           </div>
           <div>
             <font-awesome-icon icon="fa-solid fa-hashtag" class="icon" />
             <label>나이</label>
-            <input type="number" v-model="user.number" placeholder="-" disabled>
+            <input type="number" v-model="user.number" placeholder="-" :disabled="defaultMode">
           </div>
         </div>
-        
+
       </div>
     </div>
 
-    <div class="log-container">
+    <!-- <div class="log-container">
       <h2>SEARCH LOG</h2>
-    </div>
+    </div> -->
   </main>
 </template>
 
 <script>
-import { getUserInfo } from "@/api/user";
+import { getUserInfo, modifyUser, withdraw } from "@/api/user";
 import { mapActions, mapState } from "vuex";
 import RoundButton from "@/components/common/RoundButton.vue";
 
@@ -77,12 +93,41 @@ export default {
   },
   methods: {
     ...mapActions("UserStore", ["logout"]),
-  },
-  computed: {
-    ...mapState("UserStore", ["userInfo"]),
-  },
-  async created() {
-    const resolve = (res) => {
+    setMode(mode) {
+      this.mode = mode;
+    },
+    async modifyUser() {
+      const resolve = () => {
+        alert("변경되었습니다!");
+        this.mode = "default";
+      }
+      const reject = (err) => {
+        console.log(err);
+        alert("변경을 실패하였습니다!");
+      }
+      let body = {};
+      this.user.gender = 0; // 성별의 경우 0도 값이므로 추가
+      this.user.age = 0;
+      for(let item in this.user) {
+        if(this.user[item]) body[item] = this.user[item];
+      }
+      await modifyUser(body, resolve, reject);
+    },
+    async deleteUser() {
+      if(!confirm("정말로 탈퇴하시겠습니까?")) return;
+
+      const resolve = () => {
+        alert("탈퇴되었습니다!");
+        this.$store.commit("LOGOUT");
+        this.$router.push("/");
+      }
+      const reject = () => {
+        alert("회원탈퇴에 실패하였습니다!");
+      }
+      await withdraw(this.user.id, resolve, reject);
+    },
+    async initUserData() {
+      const resolve = (res) => {
       // 성공시 받아온 데이터 저장
       let {id, email, name, age, gender } = res.data;
       this.user = {id, email, name, age, gender};
@@ -97,6 +142,30 @@ export default {
     }
 
     await getUserInfo(resolve, reject);
+    }
+  },
+  computed: {
+    ...mapState("UserStore", ["userInfo"]),
+    defaultMode() {
+      return this.mode==='default';
+    },
+    genderString() {
+      return this.user.age==1?`남자`:this.user.age==2 ?`여자`: ``;
+    }
+  },
+  created() {
+    this.initUserData();
+  },
+  watch: {
+    userInfo() {
+      if(this.userInfo) {
+        this.logout();
+        this.$router.push('/');
+      }
+    },
+    mode() {
+      this.initUserData();
+    }
   }
 }
 </script>
@@ -104,14 +173,26 @@ export default {
 <style scoped>
 main {
   margin: 20px auto;
+  min-height: 60vh;
+  height: 100%;
   max-width: 1080px;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: visible;
+}
+main::before {
+  content: "";
+  position: absolute;
+  background-color: var(--navy);
+  top:0;
+  width: 100vw;
+  height: 100vh;
 }
 main > div {
   border: 1px solid var(--gray);
   display: flex;
   flex-direction: column;
-  padding: 20px;
   margin: 10px;
 }
 .user-img {
@@ -129,11 +210,18 @@ main > div {
 .info-container {
   width: 66%;
   position: relative;
+  background-color: var(--white);
 }
 .info-container .flex {
+  padding: 20px;
   display: flex;
   justify-content:space-between;
   align-items: center;
+}
+.info-container h2 {
+  margin: 0;
+  padding: 20px;
+  outline: 1px solid var(--gray);
 }
 .log-container {
   width: 33%;
@@ -164,6 +252,20 @@ main > div {
   font-size: 18px;
   color: var(--navy);
   font-weight: 700;
+  width: 200px;
+}
+.input-conatiner input {
+  border: 1px solid var(--darkgray);
+  width: 200px;
+  font-size: 18px;
+  color: var(--navy);
+}
+.input-conatiner input[type="radio"] {
+  width: auto;
+  margin-right: 20px;
+}
+.radio {
+  color: var(--navy);
 }
 .btn-container {
   position: absolute;
